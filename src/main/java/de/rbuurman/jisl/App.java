65bd5dba063d2ceaaf83f292ed581cc858f6c419
Rasmus Buurman <dev@rbuurman.de>
2024-06-11 20:45:34 +0200

    Rename TokenCursor to Lexer

--- src/main/java/de/rbuurman/jisl/App.java
+++ src/main/java/de/rbuurman/jisl/App.java
@@ -2,13 +2,13 @@ package de.rbuurman.jisl;
 
 import java.util.Stack;
 
-import de.rbuurman.jisl.parsing.lexing.TokenCursor;
+import de.rbuurman.jisl.parsing.lexing.Lexer;
 import de.rbuurman.jisl.parsing.lexing.token.Token;
 
 public class App {
     public static void main(String[] args) {
         final String text = "; comment\nhahaha";
-        final Stack<Token> tokens = TokenCursor.tokenize(text);
+        final Stack<Token> tokens = Lexer.tokenize(text);
         for (Token token : tokens) {
             System.out.println(token);
         }
--- src/main/java/de/rbuurman/jisl/parsing/lexing/Lexer.java
+++ src/main/java/de/rbuurman/jisl/parsing/lexing/Lexer.java
@@ -2,18 +2,70 @@ package de.rbuurman.jisl.parsing.lexing;
 
 import java.util.Stack;
 
-public class Lexer {
+import de.rbuurman.jisl.parsing.lexing.matcher.CharMatcher;
+import de.rbuurman.jisl.parsing.lexing.matcher.InverseMatcher;
+import de.rbuurman.jisl.parsing.lexing.matcher.LineMatcher;
+import de.rbuurman.jisl.parsing.lexing.matcher.WhitespaceMatcher;
+import de.rbuurman.jisl.parsing.lexing.token.CommentToken;
+import de.rbuurman.jisl.parsing.lexing.token.PrimitiveToken;
+import de.rbuurman.jisl.parsing.lexing.token.StringToken;
+import de.rbuurman.jisl.parsing.lexing.token.Token;
 
+public class Lexer extends Cursor {
 	public Lexer(String text) {
-
+		super(text);
 	}
 
-	public Stack<Token> tokenize() {
-		// TODO
-		return null;
+	public static Stack<Token> tokenize(String text) {
+		final var cursor = new Lexer(text);
+		var tokens = new Stack<Token>();
+
+		while (true) {
+			var token = cursor.advance();
+			final boolean exit = token.exit();
+			tokens.push(token);
+
+			if (exit)
+				break;
+		}
+
+		return tokens;
 	}
 
-	private Token advanceToken() {
-		return null;
+	public Token advance() {
+		this.eat(new WhitespaceMatcher());
+
+		switch (this.peek()) {
+			case ';':
+				this.eat(new CharMatcher(';'));
+				this.eat(new WhitespaceMatcher());
+				final String comment = this.eat(new LineMatcher());
+				return new CommentToken(comment);
+
+			case '"':
+				this.bump();
+				final String string = this.eat(new InverseMatcher(new CharMatcher('"')));
+				final char closingDel = this.bump();
+				assert closingDel == '"';
+				return new StringToken(string);
+
+			case '(':
+				return PrimitiveToken.PAREN_OPEN;
+			case ')':
+				return PrimitiveToken.PAREN_CLOSE;
+			case '[':
+				return PrimitiveToken.BRACKET_OPEN;
+			case ']':
+				return PrimitiveToken.BRACKET_CLOSE;
+			case '+':
+				return PrimitiveToken.PLUS;
+			case '-':
+				return PrimitiveToken.MINUS;
+
+			default:
+				break;
+		}
+
+		return PrimitiveToken.EOF;
 	}
 }
