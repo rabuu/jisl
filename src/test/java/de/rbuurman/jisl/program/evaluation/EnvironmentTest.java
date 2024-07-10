ce1ad9d7287c7e2091649f7f92f6b9bec98ee7f6
schmalph <pschmale@uni-heidelberg.de>
2024-07-10 21:54:53 +0200

    Expanded EnvironmentTest.

--- src/test/java/de/rbuurman/jisl/program/evaluation/EnvironmentTest.java
+++ src/test/java/de/rbuurman/jisl/program/evaluation/EnvironmentTest.java
@@ -10,15 +10,47 @@ import de.rbuurman.jisl.program.VariableName;
 
 public class EnvironmentTest {
 
+
         @Test
-        public void generalFunctionality() throws EvaluationException {
+	public void getValueTest() {
                 Environment environment = new Environment();
 
-                final var PI = new VariableName("pi", null);
-                environment.addDefinition(PI, new NumberPrimitive(3.));
+                environment.addDefinition("pi", new NumberPrimitive(3.));
 
-                assertEquals(new NumberPrimitive(3.), environment.getValue(PI));
+                assertEquals(new NumberPrimitive(3.), environment.getValue("pi"));
                 environment.reset();
-                assertThrows(EvaluationException.class, () -> environment.getValue(PI));
+                assertThrows(EvaluationException.class, () -> environment.getValue("pi"));
+	}
+
+        @Test
+        public void structsTest() {
+                Environment environment = new Environment();
+
+                Multiple<VariableName> fieldnames = new Multiple<VariableName>;
+                fields.add("fieldname1").add("fieldname2");
+                environment.addDefinition("structname", new NumberPrimitive(0.));
+
+                assertThrows(EvaluationException.class, () -> environment.addStruct("structname", fieldnames));
+
+                environment.reset();
+
+                environment.addStruct("structname", fieldnames);
+                assertEquals(fieldnames, environment.getStructFields("structname"));
+
+                assertThrows(EvaluationException.class, () -> environment.addDefinition("make-structname", new NumberPrimitive(0.)));
+
+                environment.reset();
+
+                environment.addDefinition("structname", new NumberPrimitive(0.));
+                assertThrows(EvaluationException.class, () -> environment.addStruct("structname", fieldnames));
+        }
+
+        @Test
+        public void mergeTest() {
+                Environment base = new Environment();
+                Environment local = new Environment();
+
+                base.addDefinition("pi", new Numberprimitive(3.));
+                local.addDefinition("e", new Numberprimitive(2.));
         }
 }
