1200b48f750fead8421473f387c22786921928c7
Rasmus Buurman <dev@rbuurman.de>
2024-07-13 12:52:31 +0200

    Comment stdlib.rkt

--- stdlib.rkt
+++ stdlib.rkt
@@ -1,11 +1,14 @@
 ;; Standard structs
+
 (define-struct posn (x y))
 
 ;; Constants
+
 (define e (exp 1))
 (define pi 245850992/78256779)
 
 ;; arithmetic helpers
+
 (define (positive? x)
   (> x 0))
 
@@ -21,31 +24,39 @@
 (define (odd? x)
   (= (modulo x 2) 1))
 
+; signum of a number
 (define (sgn x)
   (if (positive? x) 1 -1))
 
+; absolute value of a number
 (define (abs x)
   (if (negative? x) (- x) x))
 
+; square of a number
 (define (sqr x)
   (* x x))
 
+; increment x by 1
 (define (add1 x)
   (+ x 1))
 
+; decrement x by 1
 (define (sub1 x)
   (- x 1))
 
-;; more arithmetic functions
+;; more complex arithmetic functions
+
+; greatest common divisor
 (define (gcd x y)
   (if (= y 0)
     x
     (gcd y (modulo x y))))
 
+; least common multiple
 (define (lcm x y)
     (/ (* x y) (gcd x y)))
 
-;; list helpers
+;; list selectors
 
 (define (caar lst)
   (car (car lst)))
@@ -124,14 +135,14 @@
 
 ;; higher-order functions
 
-; Return x for every x
+; return x for every x
 (define (identity x) x)
 
-; Composes two functions into a single function
+; composes two functions into a single function
 (define (compose g f)
   (lambda (x) (g (f x))))
 
-; Constructs a list from all those items on a list for which p? holds
+; constructs a list from all those items on a list for which p? holds
 (define (filter p? lst)
   (if (empty? lst)
     empty
@@ -139,47 +150,48 @@
       (if (p? (first lst)) (list (first lst)) empty)
       (filter p? (rest lst)))))
 
-; Fold a list with a given operation from the right
+; fold a list with a given operation from the right
 (define (foldr op neutrum lst)
   (if (empty? lst) neutrum
     (op (first lst) (foldr op neutrum (rest lst)))))
 
-; Fold a list with a given operation from the left
+; fold a list with a given operation from the left
 (define (foldl op neutrum lst)
   (local [(define (fold lst acc)
             (if (empty? lst) acc
               (fold (rest lst) (op (first lst) acc))))]
     (fold lst neutrum)))
 
-; Apply an operation on each element of a list
+; apply an operation on each element of a list
 (define (map op lst)
   (if (empty? lst) empty
     (cons (op (first lst)) (map op (rest lst)))))
 
-; Determines whether p? holds for every item of lst
+; determines whether p? holds for every item of lst
 (define (andmap p? lst)
   (if (empty? lst)
     #true
     (and (p? (first lst)) (andmap p? (rest lst)))))
 
-; Determines whether p? holds for at least one item of lst
+; determines whether p? holds for at least one item of lst
 (define (ormap p? lst)
   (if (empty? lst)
     #false
     (or (p? (first lst)) (ormap p? (rest lst)))))
 
-;; some more list helpers using higher-order functions
+;; list helpers using higher-order functions
 
-; Get length of a list
+; get length of a list
 (define (length lst)
   (foldr (lambda (x y) (add1 y)) 0 lst))
 
-; Reverse a list
+; reverse a list
 (define (reverse lst)
   (foldl cons empty lst))
 
-; Returns #true if lst contains x
+; returns #true if lst contains x
 (define (member x lst)
   (ormap (lambda (y) (equal? x y)) lst))
 
+; alias member? to match boolean syntax
 (define member? member)
